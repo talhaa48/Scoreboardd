1daa50f1540ae879aca6308d6c52f282868bc13b
talhaa48 <talhasahbaz68@gmail.com>
2022-12-25 19:27:42 +0300

    Scoreboard

---
+++ index.js
@@ -0,0 +1,35 @@
+let score = 0
+let score2 = 0
+countEl = document.getElementById("home-count")
+countEl2 = document.getElementById("guest-count")
+
+function count1(){
+    score+=1
+    countEl.textContent = score
+   
+}
+function count2(){
+    score+=2
+    countEl.textContent = score
+   
+}
+function count3(){
+    score+=3
+    countEl.textContent = score
+  
+}
+function count4(){
+    score2+=1
+    countEl2.textContent = score2
+    
+}
+function count5(){
+    score2+=2
+    countEl2.textContent = score2
+    
+}
+function count6(){
+    score2+=3
+    countEl2.textContent = score2
+    
+}
